168d4a893f3daf346cf835e8d6853cda871e8d9d
Omafovbe I <i.omafovbe@gmail.com>
2022-10-15 20:55:34 +0100

    feat: :sparkles: Auth pages
    Created login and register pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Switch, Route } from "react-router-dom";
 import "./styles/tailwind.output.css";
 import LandingPage from './pages/index';
 import AboutUs from './pages/about-us';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
@@ -12,8 +14,14 @@ function App() {
       <Route exact path={`${process.env.PUBLIC_URL}/`}>
         <LandingPage />
       </Route>
-      <Route path="/about-us">
+      {/* <Route path="/about-us">
         <AboutUs />
+      </Route> */}
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route path="/sign-up">
+        <RegisterPage />
       </Route>
     </Switch>
   );
